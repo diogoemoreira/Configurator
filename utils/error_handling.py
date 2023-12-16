98d1aa32616e9fd241ffad187a86ffb204715b65
diogoemoreira <diogo.e.moreira@hotmail.com>
2023-12-16 16:01:58 +0000

    Added custom error messages
    Error messages were added for:
    - singleton
    - already existing configuration
    - invalid configuration parameters
    - update of an invalid configuration
    - load of an invalid file

---
+++ utils/error_handling.py
@@ -0,0 +1,54 @@
+class SingletonAlreadyExists(Exception):
+    '''
+    Exception raised when trying to create a singleton object which already exists
+    '''
+
+    def __init__(self):
+        self.message = "The object you are trying to create is a singleton and is already instantiated."
+    
+    def __str__(self):
+        return self.message
+    
+class ConfigurationAlreadyExists(Exception):
+    '''
+    Exception raised when trying to create a new configuration using an existing configuration's name
+    '''
+
+    def __init__(self):
+        self.message = "The name you are trying to use already exists. Please choose a new name for the new configuration or update the already existing one."
+
+    def __str__(self):
+        return self.message
+    
+class InvalidConfigurationParameters(Exception):
+    '''
+    Exception raised when a new configuration's parameters don't follow the stipulated format
+    '''
+
+    def __init__(self):
+        self.message = "The format you are trying to use is invalid. Please use the following format:\nFor a single parameter:\nkey:value\n\nFor multiple parameters:\nkey:value,key:value"
+
+    def __str__(self):
+        return self.message
+    
+class UpdateInvalidConfiguration(Exception):
+    '''
+    Exception raised when trying to update the configuration using a nonexistent configuration's name
+    '''
+
+    def __init__(self):
+        self.message = "The configuration you are trying to update does not exist. Please use an existing configuration's name."
+
+    def __str__(self):
+        return self.message
+
+class LoadInvalidFile(Exception):
+    '''
+    Exception raised when trying to load an invalid file
+    '''
+
+    def __init__(self):
+        self.message = "The file you are trying to load does not exist or has an invalid format."
+
+    def __str__(self):
+        return self.message
